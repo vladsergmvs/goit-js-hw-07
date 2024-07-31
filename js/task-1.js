9ac70790ee82332d67d7e5a6d0a13563bc7124f2
Volodymyr <vladsergmvs@gmail.com>
2024-07-31 12:01:50 +0300

    add1

--- js/task-1.js
+++ js/task-1.js
@@ -12,14 +12,33 @@ listOfItems.forEach(element =>
 }
     );
 
-//////////////////////////////////////////////////////////////////
+///////////////////////////////====================///////////////////////////////////
 
 const styles = document.createElement('style');
 styles.textContent =
-  ` #categories{
-      
+   `
+   ul{
+          padding-left: 0;
+      } 
+  #categories{
+         list-style-type: none;
+       padding: 24px;
+        width: 440px;
     }
-       
+   #categories .item{
+        background: #F6F6FE;
+        
+        padding: 20px;
+         margin: 20px;
+     }
+   #categories ul{
+         list-style-type: none;
+         }
+   #categories .item li{
+        border: 1px solid rgba(46, 47, 66, 0.4);
+        border-radius: 4px;
+        margin: 10px;
+     }
  `;
 document.head.append(styles);
 
